551b0ed1df90754fc797df6c67efd15c892e4e15
Jorge <jorge_fernandez_84@yahoo.es>
2024-01-03 17:29:05 +0100

    Update API_PARQUES
    Reestructuración del script.
    
    -Obtención zonas verdes saneadas
    -Obtención mapa zonas caninas

--- API_PARQUES.py
+++ API_PARQUES.py
@@ -5,7 +5,6 @@ import geopandas as gpd
 import json
 from shapely.geometry import Polygon, MultiPolygon
 
-# Funciones Auxiliares (Comunes en ambos scripts)
 def convertir_a_string_si_es_necesario(valor):
     if isinstance(valor, (list, dict)):
         return json.dumps(valor)
@@ -19,13 +18,13 @@ def preparar_para_geojson(dataframe):
 
 # Función optimizada para preparar los datos
 def preparar_datos(gdf):
-    columnas_relevantes = ['name', 'type', 'surface', 'landuse', 'leisure', 'geometry', 'area', 'wheelchair']
+    columnas_relevantes = ['name', 'natural', 'landuse', 'leisure', 'geometry', 'area']
     gdf_filtrado = gdf[columnas_relevantes].copy()
 
     return gdf_filtrado
 
 # Funciones Específicas para Parques
-def buscar_parques_jardines(lugar):
+def buscar_parques(lugar):
     parques_y_jardines = ox.features_from_place(lugar, {
         'leisure': ['park', 'nature_reserve', 'dog_park'],
         'landuse': ['forest', "village_green", "grass"],
@@ -38,11 +37,23 @@ def buscar_parques_jardines(lugar):
     
     return parques_y_jardines
 
-def agregar_columna_tipo_y_id_parques(dataframe):
-    dataframe['tipo_jardin'] = dataframe.apply(lambda x: x['leisure'] if pd.notna(x['leisure']) else 
-                                            (x['landuse'] if pd.notna(x['landuse']) else x['natural']), axis=1)
+def agregar_columna_tipo_y_id(dataframe, tipo_columna='tipo_jardin'):
+    # Verificar si las columnas existen antes de acceder a ellas y asignar el tipo de área
+    dataframe[tipo_columna] = dataframe.apply(lambda x: x['leisure'] if 'leisure' in x and pd.notna(x['leisure']) else 
+                                            (x['landuse'] if 'landuse' in x and pd.notna(x['landuse']) else 
+                                            (x['natural'] if 'natural' in x and pd.notna(x['natural']) else None)), axis=1)
+    
+    # Asignar un ID incremental a cada fila
     dataframe['id'] = range(1, len(dataframe) + 1)
+    
+    # Calcular el área de los polígonos en metros cuadrados y asignarla a una nueva columna
+    # Hacer una copia para cambiar el CRS solo para el cálculo del área
+    dataframe_crs = dataframe.copy().to_crs(epsg=3857)
+    dataframe['area'] = dataframe_crs.area
+    dataframe_crs = None  # Eliminar la copia para liberar memoria
+
     return dataframe
+
 
 def agregar_poligonos_al_mapa(mapa, dataframe, color, opacidad):
     for area in dataframe.itertuples():
@@ -64,25 +75,67 @@ def agregar_poligonos_al_mapa(mapa, dataframe, color, opacidad):
         geojson.add_child(popup)
         geojson.add_to(mapa)
 
+def crear_mapa_de_dog_parks(dataframe, color_dog_park='red', opacidad=0.4, archivo_mapa='mapa_dog_parks.html'):
+    # Filtrar para obtener solo dog parks
+    dog_parks = dataframe[dataframe['leisure'] == 'dog_park']
+
+    # Crear un mapa centrado en las coordenadas promedio de los dog parks
+    if not dog_parks.empty:
+        centro = dog_parks.geometry.unary_union.centroid
+        mapa_dog_parks = folium.Map(location=[centro.y, centro.x], zoom_start=15)
+    else:
+        print("No se encontraron dog parks en el DataFrame proporcionado.")
+        return
+
+    for index, area in dog_parks.iterrows():
+        if not isinstance(area.geometry, (Polygon, MultiPolygon)) or area.geometry.is_empty:
+            continue
+
+        nombre = area['name'] if pd.notna(area['name']) else 'Parque para Perros'
+        id_area = area['id']
+        area_m2 = area['area'] if 'area' in area else None
+
+        popup_text = f"ID: {id_area}<br>Nombre: {nombre}<br>Área: {area_m2:.2f} m²" if area_m2 else f"ID: {id_area}<br>Nombre: {nombre}"
+        popup = folium.Popup(popup_text, parse_html=True)
+        geojson = folium.GeoJson(
+            area.geometry.__geo_interface__,
+            style_function=lambda feature: {
+                'fillColor': color_dog_park,
+                'color': color_dog_park,
+                'weight': 1,
+                'fillOpacity': opacidad
+            }
+        )
+        geojson.add_child(popup)
+        geojson.add_to(mapa_dog_parks)
+
+    # Guardar el mapa en un archivo HTML
+    mapa_dog_parks.save(archivo_mapa)
+
 def guardar_parques_en_csv_y_geojson(parques_y_jardines):
     parques_y_jardines_preparados = preparar_para_geojson(parques_y_jardines.copy())
-    parques_y_jardines_preparados.to_csv('parques_y_jardines.csv', index=False)
-    parques_y_jardines_preparados.to_file('parques_y_jardines.geojson', driver='GeoJSON')
-
-# Función Principal
+    parques_y_jardines_preparados.to_csv('parques.csv', index=False)
+    parques_y_jardines_preparados.to_file('parques.geojson', driver='GeoJSON')
+    
 def main():
     lugar = "Burgos, Spain"
-    parques_y_jardines = buscar_parques_jardines(lugar)
+    parques_y_jardines = buscar_parques(lugar)
     parques_y_jardines_gdf = gpd.GeoDataFrame(parques_y_jardines, crs="EPSG:4326")
-    parques_y_jardines_gdf = agregar_columna_tipo_y_id_parques(parques_y_jardines_gdf)
-
+    
     parques_y_jardines_gdf = preparar_datos(parques_y_jardines_gdf)
-
-    guardar_parques_en_csv_y_geojson(parques_y_jardines_gdf)
-
+    parques_y_jardines_gdf = agregar_columna_tipo_y_id(parques_y_jardines_gdf, 'tipo_jardin')
+    
+    # Crear el mapa
     mapa_parques = folium.Map(location=[42.3439, -3.6969], zoom_start=13)
+
+    # Agregar polígonos al mapa asegurándote de que solo se agregan una vez con el color correcto
     agregar_poligonos_al_mapa(mapa_parques, parques_y_jardines_gdf, 'green', 0.4)
+
+    # Guardar el mapa
     mapa_parques.save('mapa_parques.html')
+    
+    # Crear un mapa específico para dog parks
+    crear_mapa_de_dog_parks(parques_y_jardines_gdf)
 
 if __name__ == "__main__":
     main()
